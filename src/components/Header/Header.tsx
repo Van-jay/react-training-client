94cfee92c0b2503369a8308527fbbc3ccdbcd1fa
Van-jay <ivan.antsimonik@gmail.com>
2021-04-17 12:07:25 +0300

    update styles path

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../Button/Button';
 import styles from './Header.module.css';
-import globalStyles from '../../../styles/global.module.css';
+import globalStyles from '../../styles/global.module.css';
 import { useHistory } from 'react-router-dom';
 
 const Header = () => {
