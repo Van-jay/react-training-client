2ced84196ff968cfd0a0a352b439cb96ba0ebb37
Ivan Antsimonik <ivan.antsimonik@gmail.com>
2021-05-04 09:52:39 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
+import ErrorBoundary from './hoc/ErrorBoundary';
 import './index.css';
 import { store } from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
